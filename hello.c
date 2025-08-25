fa91a7be7ff015c3ee770c50fd304827d7a6abe3
ArjunSharma012 <arjunsharma20612@gmail.com>
2025-08-25 09:40:51 +0530

    Create hello.c
    my first code

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main() {
+  int A,B,C;
+  printf("enter three side :");
+  scanf("%d %d %d", &A, &B, &C);
+
+  if (A==B==C)
+  {
+    printf("equaletral triangle");
+  }
+  
+    else (A!=B!=C);
+    {
+      printf("scalene");
+    }
+  
+    
+  
+  return 0;
+}
